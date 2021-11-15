e2885ef9328b2d4756fd6eefa15ecb9f0b656fce
dgeddesICG <d.geddes.1@research.gla.ac.uk>
2021-11-15 14:31:39 +0000

    Made file handling more robust

--- H5_Extractor.m
+++ H5_Extractor.m
@@ -7,11 +7,26 @@
 %Written by Daniel Geddes, d.geddes.1@research.gla.ac.uk
 
 clear all;
+
+%% Set up directories
+
+file_dir = 'C:\Users\Daniel Geddes\OneDrive - University of Glasgow\UCL';
+datafile = 'h5-D.h5';
+data_dir = [file_dir '\' datafile];
+
+%Sets up new folder which will contain data extracted from the H5
+[~, filename, ~] = fileparts(data_dir);
+sv_dir = [file_dir '\' filename '_DataFiles'];
+
+if ~exist(sv_dir, 'dir')
+    mkdir(sv_dir)
+end
+
+
 %% Open H5 and strip metadata
 
-datafile = ['h5-FAD.h5'];
 
-info = h5info(datafile);
+info = h5info(data_dir);
 for k = 1 : size(info.Groups.Attributes,1)
     if  info.Groups.Attributes(k).Name == "Width" %|| info.Groups.Attributes(k).Name =="ImageWidth"
         Image_Width = info.Groups.Attributes(k).Value;
@@ -29,15 +44,12 @@ end
 
 %% Extract FLIM + intensity data
 
-Histogram_data = h5read(datafile,'/FLIM/Histogram');
-
-
-
-Intensity_data = h5read(datafile,'/FLIM/Intensity');
-FWHM_data = h5read(datafile, '/FLIM/FWHM');
-Lifetime_data = h5read(datafile, '/FLIM/Lifetime');
-Snapshot_data = h5read(datafile, '/FLIM/Snapshot');
-Sum_data = h5read(datafile, '/FLIM/Sum');
+Histogram_data = h5read(data_dir,'/FLIM/Histogram'); %TCSPC data
+Intensity_data = h5read(data_dir,'/FLIM/Intensity'); %Autofluorescence data
+FWHM_data = h5read(data_dir, '/FLIM/FWHM'); %Full-width half-max
+Lifetime_data = h5read(data_dir, '/FLIM/Lifetime'); %Crude single exponential fit exported from EZtime
+Snapshot_data = h5read(data_dir, '/FLIM/Snapshot'); %Not sure what this represents but EzTime includes it in the export
+Sum_data = h5read(data_dir, '/FLIM/Sum'); %Sum of all histograms
 
 %Reshape data files in to appropriate format
 FWHM_data = reshape(FWHM_data', [Image_Width Image_Height]);
@@ -47,23 +59,16 @@ Intensity_data = reshape(Intensity_data', [Image_Width Image_Height]);
 info_data = [Image_Width Image_Height Num_of_Bins Time_Per_Bin_ps]; %Note TimePerBin has been converted to PicoSeconds
 
 
-%% Save Data as txt files for FLIM_Fit
-
-%Sets up new folder which will contain data extracted from the H5
-[~, filename, ~] = fileparts(datafile);
-  if ~exist([filename '_DataFiles'], 'dir')
-       mkdir([filename '_DataFiles'])
-  end
-data_dir = [filename '_DataFiles'];
+%% Save Data as txt files
 
 %Saves the data in tab delimited txt files to the newly created folder
-writematrix(Histogram_data, [cd '/' data_dir '/' filename '_Histogram_Data'], 'Delimiter', 'tab')
-writematrix(FWHM_data, [cd '/' data_dir '/' filename '_FWHM_Data'], 'Delimiter', 'tab')
-writematrix(Lifetime_data, [cd '/' data_dir '/' filename '_Lifetime_Data'], 'Delimiter', 'tab')
-writematrix(Snapshot_data, [cd '/' data_dir '/' filename '_Snapshot_Data'], 'Delimiter', 'tab')
-writematrix(Sum_data, [cd '/' data_dir '/' filename '_SUM_Data'], 'Delimiter', 'tab')
-writematrix(Intensity_data, [cd '/' data_dir '/' filename '_Intensity_Data'], 'Delimiter', 'tab')
+writematrix(Histogram_data, [sv_dir '\' filename '_Histogram_Data'], 'Delimiter', 'tab')
+writematrix(FWHM_data, [sv_dir '\' filename '_FWHM_Data'], 'Delimiter', 'tab')
+writematrix(Lifetime_data, [sv_dir '\' filename '_Lifetime_Data'], 'Delimiter', 'tab')
+writematrix(Snapshot_data, [sv_dir '\' filename '_Snapshot_Data'], 'Delimiter', 'tab')
+writematrix(Sum_data, [sv_dir '\' filename '_SUM_Data'], 'Delimiter', 'tab')
+writematrix(Intensity_data, [sv_dir '\' filename '_Intensity_Data'], 'Delimiter', 'tab')
 
 %File containing properties of the data sets
-writematrix(info_data, [cd '/' data_dir '/' filename '_InfoFile'], 'Delimiter', 'tab')
+writematrix(info_data, [sv_dir '\' filename '_InfoFile'], 'Delimiter', 'tab')
 
